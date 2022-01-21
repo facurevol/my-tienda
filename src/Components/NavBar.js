f075a3e144d2b2c31a4745705064b7c366747418
facurevol <facundorevol@outlook.com>
2022-01-21 08:56:50 -0300

    ajustes en NavBar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,23 +1,35 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from 'react';
-import './NavBar.css';
-
+import React from "react";
+import "./NavBar.css";
+import logo from "./../img/casaPelgo.svg";
 export default function NavBar() {
-  return <div className='nav'>
-    <div className='imgNavBar'><img src='public/casaPelgo.svg' alt="Casa Pelgo" /></div>
-    <ul className="nav">
-      <li className="nav">
-        <a className="nav" href="#!">Home</a>
-      </li>
-      <li className="nav">
-        <a className="nav" href="#!">Accesorios</a>
-      </li>
-      <li className="nav">
-        <a className="nav" href="#!">Deco</a>
-      </li>
-      <li className="nav">
-        <a className="nav" href="#!">Novedades</a>
-      </li>
-    </ul>
-  </div>
+  return (
+    <div className="nav">
+      <div className="imgNavBar">
+        <img src={logo} alt="Casa Pelgo" />
+      </div>
+      <ul className="nav">
+        <li className="nav">
+          <a className="nav" href="#!">
+            Home
+          </a>
+        </li>
+        <li className="nav">
+          <a className="nav" href="#!">
+            Accesorios
+          </a>
+        </li>
+        <li className="nav">
+          <a className="nav" href="#!">
+            Deco
+          </a>
+        </li>
+        <li className="nav">
+          <a className="nav" href="#!">
+            Novedades
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
